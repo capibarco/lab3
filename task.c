d387607d23d6ce5efd08ac9e095206bbec7000a6
сapi <115636784+capibarco@users.noreply.github.com>
2023-04-01 02:31:37 +0700

    Update task.c

--- task.c
+++ task.c
@@ -120,7 +120,6 @@ int main(int argc, char** argv)
 	cublasDestroy(handle);
 	free(matrixOld);
 	free(matrixNew);
-	free(matrixTmp);
 	printf("iterations = %d, error = %lf, time = %lf\n", iterNow, errorNow, (double)(end - begin) / CLOCKS_PER_SEC);
 
 	return 0;
