095cdb9f57185363db5ce89e18ab517f131eff2f
сapi <115636784+capibarco@users.noreply.github.com>
2023-04-01 01:07:12 +0700

    Update task.c

--- task.c
+++ task.c
@@ -80,6 +80,7 @@ int main(int argc, char** argv)
 		matrixNew[size * i + size - 1] = matrixOld[size * i + size - 1];
 		matrixNew[size * (size - 1) + i] = matrixOld[size * (size - 1) + i];
 	}
+	#pragma acc kernel
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -90,6 +91,7 @@ int main(int argc, char** argv)
 	{
 		iterNow++;
 		matrixCalc(size);
+		#pragma acc kernel
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++)
